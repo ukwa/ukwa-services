cb4b1b2a39a4150c1ffee7404b0431e84518e430
Andrew Jackson <Andrew.Jackson@bl.uk>
2020-04-14 14:17:14 +0100

    Removed old files, made config switchable.

--- ingest/fc/beta/docker-beta.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Pull in configuration environment variables:
-source /etc/sysconfig/w3act
-
-# Launch with correct combined configuration:
-docker-compose -f ../docker-compose.yml -f docker-compose.beta.yml -p pulse_beta "$@"
