87f9d21b8338bc5c7e9118acfcf18571e24a1213
Andrew Jackson <Andrew.Jackson@bl.uk>
2020-05-01 20:44:53 +0100

    Added script to update allows from W3ACT DB dump on HDFS.

--- access/website/scripts/w3act/update-acl.sh
+++ access/website/scripts/w3act/update-acl.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 
 # Source env
-source ~/gitlab/ukwa-services-env/w3act/dev/w3act.env
+export HDFS_W3ACT_DB_CSV=/9_processing/w3act/w3act-db-csv.zip
 export ALLOWS_FILE=/mnt/nfs/data/website/open-access-acl/allows.aclj
 
 # Dump W3ACT DB as CSV
-echo "Dumping W3ACT database to CSV..."
-docker run -i -v $PWD/w3act-db-csv:/w3act-db-csv ukwa/python-w3act w3act -d /w3act-db-csv get-csv -H 192.168.45.60 -P 5434 -p $W3ACT_PSQL_PASSWORD
+echo "Downloading W3ACT CSV from Hadoop..."
+rm -f w3act-db-csv.zip w3act-db-csv/*.*
+docker run -ti -v $PWD:/host anjackson/ukwa-manage:trackdb-lib store get $HDFS_W3ACT_DB_CSV /host/w3act-db-csv.zip 
+unzip -o w3act-db-csv.zip
 
 # Generate the OA access list
 echo "Generating open-access allow list..."
-docker run -i -v $PWD/w3act-db-csv:/w3act-db-csv ukwa/python-w3act w3act -d /w3act-db-csv gen-acl /w3act-db-csv/allows.aclj
+docker run -i -v $PWD:/host ukwa/python-w3act w3act -d /host/w3act-db-csv gen-acl /host/allows.aclj
 
 # Copy the allows, and then update atomically:
 echo "Updating allow list at ${ALLOWS_FILE}"
-cp $PWD/w3act-db-csv/allows.aclj ${ALLOWS_FILE}.new
+cp allows.aclj ${ALLOWS_FILE}.new
 mv -f ${ALLOWS_FILE}.new ${ALLOWS_FILE}
