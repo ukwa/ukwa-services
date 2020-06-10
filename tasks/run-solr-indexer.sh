bf4fd6a8a927778f7f419cbf7a9cb11f7de9bc71
Andrew Jackson <Andrew.Jackson@bl.uk>
2020-06-10 19:57:46 +0100

    Updated indexer scripts.

--- tasks/run-solr-indexer.sh
+++ tasks/run-solr-indexer.sh
@@ -10,6 +10,8 @@ source ${DIR}/task.env.sh
 # Specific vars:
 export STREAM="frequent"
 export YEAR="2020"
+export SOLR_COLLECTION=fc-2020-test
+export SOLR_ZKS="dev-zk1:2182,dev-zk2:2182,dev-zk3:2182"
 
 # Ensure we only run one copy of this script at a time:
 LOCKFILE="/var/tmp/`basename $0`.lock"
@@ -19,14 +21,17 @@ unset lockfd
     # Check lock and exit if locked:
     flock -n $lockfd || { echo `basename $0` "is already running!"; exit 1; }
 
-    echo "CDX indexing WARCs from $TRACKDB_URL for ${STREAM} ${YEAR} and using ${TASK_IMG}..."
-    docker run -i $TASK_IMG windex cdx-index \
+    echo "Solr indexing WARCs from $TRACKDB_URL for ${STREAM} ${YEAR} and using ${TASK_IMG}..."
+    docker run -i $TASK_IMG -v ${SHARED}:/shared windex solr-index \
         --trackdb-url $TRACKDB_URL \
         --stream $STREAM \
         --year $YEAR \
-        --cdx-collection data-heritrix \
-        --cdx-service "http://cdx.api.wa.bl.uk" \
-        --batch-size 1000
+        --solr-collection ${SOLR_COLLECTION} \
+        --zks ${SOLR_ZKS} \
+        --batch-size 1000 \
+        /shared/warc-npld.conf \
+        /shared/annotations.json \
+        /shared/allows.txt
 
 ) {lockfd}< $LOCKFILE
 
