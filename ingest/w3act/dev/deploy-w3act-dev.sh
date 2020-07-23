738eab5952585ef81d26ab8cdd70b508152b424e
Andrew Jackson <Andrew.Jackson@bl.uk>
2020-07-23 09:51:41 +0100

    Update pywb.

--- ingest/w3act/dev/deploy-w3act-dev.sh
+++ ingest/w3act/dev/deploy-w3act-dev.sh
@@ -4,7 +4,7 @@
 source /mnt/nfs/config/gitlab/ukwa-services-env/w3act/dev/w3act.env
 
 # Pull in context variables:
-source w3act.env
+source ./w3act.env
 
 # Launch the common configuration with these environment variable:
 docker stack deploy -c ../docker-compose.yml w3act
