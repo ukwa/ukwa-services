949f8238470b0bd632bc8097d7c4b0d5995727e7
Andrew Jackson <Andrew.Jackson@bl.uk>
2020-08-27 10:30:48 +0100

    Fix up config for pywb.

--- manage/intranet/deploy-intranet-stack.sh
+++ manage/intranet/deploy-intranet-stack.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 #export SERVER_NAME=intranet.dapi.wa.bl.uk
-export SERVER_NAME=192.168.45.91:90
+export SERVER_NAME=dev1.n45.wa.bl.uk:90
 export KAFKA_BROKER=192.168.45.15:9094
+export CRAWLER_WEBHDFS=192.168.45.15:8001/by-filename/
 export DEPLOYMENT_TAG=dev
 
 docker stack deploy -c docker-compose.yml intranet
