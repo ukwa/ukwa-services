05afe889697085b527b0fb649546d2447e62bf33
EC2 Default User <ec2-user@ip-172-31-29-0.eu-west-2.compute.internal>
2020-09-07 19:58:52 +0000

    Updated to run on AWS setup.

--- ingest/dc/dc-crawl/deploy-crawl-prod.sh
+++ ingest/dc/dc-crawl/deploy-crawl-prod.sh
@@ -2,4 +2,10 @@ source ../prod.env
 
 export HERITRIX_VERSION=2.7.3
 
+mkdir -p /heritrix/output/prometheus/config
+cp prometheus/config/* /heritrix/output/prometheus/config
+mkdir -p /heritrix/output/prometheus/data
+
+mkdir ./shared
+
 docker stack deploy -c docker-compose.yml dc_crawl
--- ingest/dc/dc-kafka/deploy-kafka-prod.sh
+++ ingest/dc/dc-kafka/deploy-kafka-prod.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
-export STORAGE_PATH=/mnt/gluster/dc2019kafka
+export STORAGE_PATH=/heritrix/kafka
+
+mkdir -p ${STORAGE_PATH}/zookeeper/data
+mkdir -p ${STORAGE_PATH}/zookeeper/datalog
+
+mkdir -p ${STORAGE_PATH}/kafka/kafka-logs-broker-1
+mkdir -p ${STORAGE_PATH}/prom-jmx
+cp prom-jmx/* ${STORAGE_PATH}/prom-jmx
+
 docker stack deploy -c docker-compose.yml dc_kafka
 
