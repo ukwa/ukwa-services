0b7fd507fb4c5ee9fa0075c494e18a417aef414d
Andrew Jackson <Andrew.Jackson@bl.uk>
2020-12-04 14:34:19 +0000

    Add env setup to website tests.

--- access/website_tests/deploy-website-tests.sh
+++ access/website_tests/deploy-website-tests.sh
@@ -1,12 +1,25 @@
 #!/bin/sh
 
+# read script environ argument
+ENVIRON=$1
+if ! [[ ${ENVIRON} =~ dev|beta|prod ]]; then
+        echo "ERROR: Script $0 requires environment argument (dev|beta|prod)"
+        exit
+fi
 
-source ~/gitlab/ukwa-services-env/access/website/dev.env
-export HOST=https://dev:${DEV_WEBSITE_PW}@dev.webarchive.org.uk
+if [[ ${ENVIRON} == 'dev' ]]; then
+	# Set up the dev.webarchive.org.uk vars
+        source ~/gitlab/ukwa-services-env/access/website/dev.env
+        export HOST=https://dev:${DEV_WEBSITE_PW}@dev.webarchive.org.uk
+        export PUSH_GATEWAY=monitor-pushgateway.dapi.wa.bl.uk:80
+else
+        export PUSH_GATEWAY=monitor.wa.bl.uk:9091
+	echo "ERROR"
+	exit
+fi
 
-export PROMETHEUS_JOB_NAME=access_website_rf_tests_dev
-
-export PUSH_GATEWAY=monitor.wa.bl.uk:9091
+# Add environ tag to job name for Prometheus metrics:
+export PROMETHEUS_JOB_NAME=access_website_rf_tests_${ENVIRON}
 
 # --
 echo Running tests using HOST = $HOST
