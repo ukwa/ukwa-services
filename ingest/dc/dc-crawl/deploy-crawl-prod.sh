7ef226296240c574435f272c51c8f251c3efd1ee
EC2 Default User <ec2-user@ip-172-31-26-103.eu-west-2.compute.internal>
2021-09-30 10:38:19 +0000

    Prod config used for last week, prior to rescale.

--- ingest/dc/dc-crawl/deploy-crawl-prod.sh
+++ ingest/dc/dc-crawl/deploy-crawl-prod.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+# Shared vars:
 source ../prod.env
 
 export CURRENT_UID=$(id -u):$(id -g)
@@ -6,7 +9,15 @@ echo "Will run as ${CURRENT_UID},,,"
 
 sudo mkdir -p /heritrix/output/prometheus/config
 sudo mkdir -p /heritrix/output/prometheus/data
-sudo chown -R ec2-user /heritrix/output
+
+# Ensure Heritrix user group can write to output folder:
+sudo chown -R ec2-user:1001 /heritrix/output
+sudo chown -R ec2-user:1001 /heritrix/scratch
+sudo chown -R ec2-user:1001 /heritrix/state
+sudo chmod -R g+w /heritrix/output
+sudo chmod -R g+w /heritrix/scratch
+sudo chmod -R g+w /heritrix/state
+
 
 cp prometheus/config/* /heritrix/output/prometheus/config
 
