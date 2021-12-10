dc17dcb3375822a0bf389acb1aa9d855b74fb39c
Andrew Jackson <Andrew.Jackson@bl.uk>
2021-12-10 15:32:17 +0000

    Added TrackDB setup for DEV etc.

--- manage/trackdb/dev/start_solr_trackdb.sh
+++ manage/trackdb/dev/start_solr_trackdb.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export DATA_TRACKDB=/mnt/nfs/data/trackdata
+export DATA_TRACKDB=/mnt/nfs/data/trackdb
 
 # start the solr trackdb container
 docker stack deploy -c docker-compose.yml trackdb
