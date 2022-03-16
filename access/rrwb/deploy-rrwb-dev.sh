9db46dcbf403367cc42ffe35d412cf3d6af01b2f
Andrew Jackson <Andrew.Jackson@bl.uk>
2022-03-16 13:17:39 +0000

    Add LOCKS_AUTH settings.

--- access/rrwb/deploy-rrwb-dev.sh
+++ access/rrwb/deploy-rrwb-dev.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+# Where to store shared files:
 export STORAGE_PATH_SHARED=/mnt/nfs/data/airflow/data_exports
 
+# Username and password to use to access the locks pages:
+export LOCKS_AUTH=demouser:demopass
+
+# Which version of PyWB to use:
 export PYWB_IMAGE=ukwa/ukwa-pywb:2.6.4
 
+# Deploy as a Docker Stack
 docker stack deploy -c docker-compose.yml access_rrwb
