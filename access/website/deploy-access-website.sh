bccfa9df8d64e85e85e2f3cb84532846c5028f0c
Andrew Jackson <Andrew.Jackson@bl.uk>
2022-04-08 13:32:10 +0100

    Update to ukwa/ukwa-ui:v.1.4.0

--- access/website/deploy-access-website.sh
+++ access/website/deploy-access-website.sh
@@ -9,7 +9,7 @@ fi
 
 # Set up environment variables
 if [[ ${ENVIRON} == 'prod' ]]; then
-	export UKWA_UI_IMAGE="ukwa/ukwa-ui:v1.3.7"
+	export UKWA_UI_IMAGE="ukwa/ukwa-ui:v1.4.0"
 	export SERVER_NAME=www.webarchive.org.uk
 	export DEPLOYMENT_TAG=prod
 	export STORAGE_PATH_WEBSITE=/mnt/nfs/prod1/access/data/website
@@ -19,7 +19,7 @@ if [[ ${ENVIRON} == 'prod' ]]; then
 	source /mnt/nfs/prod1/access/gitlab/ukwa-services-env/prod.env
 
 elif [[ ${ENVIRON} == 'beta' ]]; then
-	export UKWA_UI_IMAGE="ukwa/ukwa-ui:master"
+	export UKWA_UI_IMAGE="ukwa/ukwa-ui:v1.4.0"
 	export SERVER_NAME=beta.webarchive.org.uk
 	export DEPLOYMENT_TAG=beta
 	export STORAGE_PATH_WEBSITE=/mnt/gluster/beta/ingest/data/website
@@ -30,7 +30,8 @@ elif [[ ${ENVIRON} == 'beta' ]]; then
 	source /home/ingest/gitlab/ukwa-services-env/beta.env
 else
 	# dev vars
-	export UKWA_UI_IMAGE="min2ha/ukwa-ui:new_feature_categories_anj"
+	#export UKWA_UI_IMAGE="min2ha/ukwa-ui:new_feature_categories_anj"
+	export UKWA_UI_IMAGE="ukwa/ukwa-ui:v1.4.0"
 	export SERVER_NAME=dev.webarchive.org.uk
 	export DEPLOYMENT_TAG=dev
 	export STORAGE_PATH_WEBSITE=/mnt/nfs/data/website
