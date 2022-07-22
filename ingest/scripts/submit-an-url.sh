88b2fb2b5ab7c0ce5d3c4891316bacc376a110b0
Andrew Jackson <Andrew.Jackson@bl.uk>
2022-07-22 09:28:04 +0100

    Updates to production, including for #99

--- ingest/scripts/submit-an-url.sh
+++ ingest/scripts/submit-an-url.sh
@@ -4,4 +4,4 @@ export URL=$1
 
 echo Submitting: $URL
 
-docker run -ti --net=host ukwa/crawl-streams submit -k crawler05.n45.bl.uk:9094 -F -L now fc.tocrawl.npld $URL
+docker run -ti --net=host ukwa/crawl-streams submit -k localhost:9094 -F -L now fc.tocrawl.npld $URL
