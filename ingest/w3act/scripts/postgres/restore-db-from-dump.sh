c8daa989ff324f6a5adc1bdb17a9e2ec9f73a691
gilh <gil.hoggarth@bl.uk>
2022-10-06 22:32:36 +0100

    Updated restore for Swarm mode.

--- ingest/w3act/scripts/postgres/restore-db-from-dump.sh
+++ ingest/w3act/scripts/postgres/restore-db-from-dump.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-###########################
-DEBUG=
-DBPAUSE=60
-###########################
-SLEEP=5
-
 # Common setup
 source ./common.env
 echo "DOCKER_COMMAND: [${DOCKER_COMMAND}]"
@@ -13,42 +7,17 @@ echo "W3ACT_PSQL_DIR: [${W3ACT_PSQL_DIR}]"
 echo "W3ACT_DUMPS_DIR:[${W3ACT_DUMPS_DIR}]"
 echo "W3ACT_PSQL_PASSWORD: [${W3ACT_PSQL_PASSWORD}]"
 
-# Inform
-echo Attempting to restore using file ${W3ACT_DUMPS_DIR}/w3act_dump.sql ...
+# Setup standard var
+export PGPASSWORD=$W3ACT_PSQL_PASSWORD
 
-if [[ ${DEBUG} ]]; then
-	echo "Deleting contents of previous Postgres database restore ----------------"		# DEBUG
-	echo "from ${W3ACT_PSQL_DIR}"								# DEBUG
-	[[ ${W3ACT_PSQL_DIR} ]] && rm -rf ${W3ACT_PSQL_DIR}/*					# DEBUG
-fi
-
-# (re)start postgres
-echo "Downing all docker-compose.yml containers ------------------------------"
-$DOCKER_COMMAND down
-sleep ${SLEEP}
-echo "Upping docker-compose.yml postgres container ---------------------------"
-$DOCKER_COMMAND up -d postgres
-
-echo "Pausing ${DBPAUSE} seconds for postgres to fully start -------------------"
-while [[ ${DBPAUSE} -gt 0 ]]; do
-	echo -n "${DBPAUSE} "
-	DBPAUSE=$((DBPAUSE-1))
-	sleep 1
-done
-echo 
-
-# (re)create the instance we are going to load the dumps into
-echo "dropdb ------------------------------------------------"
-$DOCKER_COMMAND exec postgres dropdb -U postgres w3act
-sleep ${SLEEP}
-#echo "createdb ----------------------------------------------"
-$DOCKER_COMMAND exec postgres createdb -U postgres w3act
-sleep ${SLEEP}
+# Drop the current DB:
+echo "Dropping current DB, creating a new empty one..."
+docker run -i --net w3act_default -v $W3ACT_DUMPS_DIR:/host -e PGPASSWORD postgres:9.6.2 dropdb -U w3act -h postgres w3act
+sleep 5
+docker run -i --net w3act_default -v $W3ACT_DUMPS_DIR:/host -e PGPASSWORD postgres:9.6.2 createdb -U w3act -h postgres w3act
 
 # restore dump into this instance
-echo "Importing data..."
-$DOCKER_COMMAND exec postgres pg_restore -v -U w3act -n public -d w3act /tmp/w3act_dump.sql
+echo "Restoring W3ACT DB to ${W3ACT_DUMPS_DIR}..."
+docker run -i --net w3act_default -v $W3ACT_DUMPS_DIR:/host -e PGPASSWORD postgres:9.6.2 pg_restore -v -U w3act -h postgres -n public -d w3act /host/w3act_dump.sql
 
-# done - we now have a postgres instance (in a volume which will persist outside this container if we've mounted it)
-echo "Shutting down..."
-$DOCKER_COMMAND down
+#  pg_restore -v -U w3act -n public -d w3act /tmp/w3act_dump.sql
