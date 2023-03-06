94a1ff92d58822c16220862d6c44fd187fa61884
Andrew Jackson <Andrew.Jackson@bl.uk>
2023-03-06 16:04:41 +0000

    Updated for DC2023 currently on crawler04.

--- ingest/dc/dc-crawl/deploy-crawl-prod.sh
+++ ingest/dc/dc-crawl/deploy-crawl-prod.sh
@@ -7,20 +7,23 @@ export CURRENT_UID=$(id -u):$(id -g)
 
 echo "Will run as ${CURRENT_UID},,,"
 
-sudo mkdir -p /heritrix/output/prometheus/config
-sudo mkdir -p /heritrix/output/prometheus/data
+sudo mkdir -p ${STORAGE_PATH}/cdx-data
+sudo mkdir -p ${STORAGE_PATH}/heritrix/state
+sudo mkdir -p ${STORAGE_PATH}/heritrix/scratch
+sudo mkdir -p ${STORAGE_PATH}/heritrix/output/prometheus/config
+sudo mkdir -p ${STORAGE_PATH}/heritrix/output/prometheus/data
 
 # Ensure Heritrix user group can write to output folder:
 echo "Ensure file ownership and permissions are correct..."
-sudo chown -R ec2-user:1001 /heritrix/output
-sudo chown -R ec2-user:1001 /heritrix/scratch
-sudo chown -R ec2-user:1001 /heritrix/state
-sudo chmod -R g+w /heritrix/output
-sudo chmod -R g+w /heritrix/scratch
-sudo chmod -R g+w /heritrix/state
+sudo chgrp -R docker ${STORAGE_PATH}/heritrix/output
+sudo chgrp -R docker ${STORAGE_PATH}/heritrix/scratch
+sudo chgrp -R docker ${STORAGE_PATH}/heritrix/state
+sudo chmod -R g+w ${STORAGE_PATH}/heritrix/output
+sudo chmod -R g+w ${STORAGE_PATH}/heritrix/scratch
+sudo chmod -R g+w ${STORAGE_PATH}/heritrix/state
 
 
-cp prometheus/config/* /heritrix/output/prometheus/config
+cp prometheus/config/* ${STORAGE_PATH}/heritrix/output/prometheus/config
 
 mkdir -p ./shared
 
--- ingest/dc/dc-kafka/deploy-kafka-prod.sh
+++ ingest/dc/dc-kafka/deploy-kafka-prod.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export CURRENT_UID=$(id -u):$(id -g)
-export STORAGE_PATH=/heritrix/kafka
+export STORAGE_PATH=/mnt/lr10/dc
 
 sudo mkdir -p ${STORAGE_PATH}/zookeeper/data
 sudo mkdir -p ${STORAGE_PATH}/zookeeper/datalog
@@ -8,7 +8,7 @@ sudo mkdir -p ${STORAGE_PATH}/zookeeper/datalog
 sudo mkdir -p ${STORAGE_PATH}/kafka/kafka-logs-broker-1
 sudo mkdir -p ${STORAGE_PATH}/prom-jmx
 
-sudo chown -R ec2-user ${STORAGE_PATH}
+#sudo chown -R ec2-user ${STORAGE_PATH}
 cp prom-jmx/* ${STORAGE_PATH}/prom-jmx
 
 docker stack deploy -c docker-compose.yml dc_kafka
